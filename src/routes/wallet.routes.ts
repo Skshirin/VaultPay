3e3c2d72c9194f49991a93cd556dc2b66930fe81
Shaikh Shirin <skshirin007@gmail.com>
2026-08-13 20:20:55 +0530

    feat: add wallet balance endpoint

--- src/routes/wallet.routes.ts
+++ src/routes/wallet.routes.ts
@@ -1,8 +1,13 @@
 import { Router } from "express";
 import { authenticate } from "../middleware/auth.middleware.js";
-import { deposit } from "../controllers/wallet.controller.js";
+import { deposit, getWalletBalance } from "../controllers/wallet.controller.js";
+
 
 const router = Router();
+
+router.post("/deposit", authenticate, deposit);
+
+router.get("/", authenticate, getWalletBalance);
 
 router.post("/deposit", authenticate, deposit);
 
